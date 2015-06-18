57b0db2e67950b9f32d219abe6fdab34f2303136
joics2008 <joics2008@gmail.com>
2015-06-18 13:50:55 -0500

    Enteprise
    Programa de prueba para talleer de Ingenieria de Software

---
+++ Actualizador de Enterprise Basico Net 4/Actualizador de Enterprise Basico/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Actualizador_de_Enterprise_Basico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkNotas = new System.Windows.Forms.CheckBox();
+            this.txtNotas = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(577, 26);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(127, 25);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "1. Buscar Ejecutable";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(577, 55);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(127, 25);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "2. Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Enabled = false;
+            this.txtRuta.Location = new System.Drawing.Point(10, 26);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(561, 20);
+            this.txtRuta.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ruta de ejecutable:";
+            // 
+            // chkNotas
+            // 
+            this.chkNotas.AutoSize = true;
+            this.chkNotas.Location = new System.Drawing.Point(10, 60);
+            this.chkNotas.Name = "chkNotas";
+            this.chkNotas.Size = new System.Drawing.Size(250, 17);
+            this.chkNotas.TabIndex = 4;
+            this.chkNotas.Text = "Agregar Notas al Ejecutable Saliente (Opcional)";
+            this.chkNotas.UseVisualStyleBackColor = true;
+            this.chkNotas.CheckedChanged += new System.EventHandler(this.chkNotas_CheckedChanged);
+            // 
+            // txtNotas
+            // 
+            this.txtNotas.AccessibleDescription = "";
+            this.txtNotas.Location = new System.Drawing.Point(255, 58);
+            this.txtNotas.MaxLength = 60;
+            this.txtNotas.Name = "txtNotas";
+            this.txtNotas.Size = new System.Drawing.Size(316, 20);
+            this.txtNotas.TabIndex = 5;
+            this.txtNotas.Visible = false;
+            this.txtNotas.TextChanged += new System.EventHandler(this.txtNotas_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 97);
+            this.Controls.Add(this.txtNotas);
+            this.Controls.Add(this.chkNotas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnBuscar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Actualizador de Enterprise Basico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkNotas;
+        private System.Windows.Forms.TextBox txtNotas;
+    }
+}
+
---
+++ Actualizador de Enterprise Basico Net 4/Actualizador de Enterprise Basico/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Actualizador_de_Enterprise_Basico
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+            }
+
+        private string nombreMes(int numeroMes) 
+        {
+            string mes = "";
+            switch (numeroMes)
+            {
+                case 1:
+                    {
+                        mes = "Ene";
+                        break;
+                    }
+                case 2:
+                    {
+                        mes = "Feb";
+                        break;
+                    }
+                case 3:
+                    {
+                        mes = "Mar";
+                        break;
+                    }
+                case 4:
+                    {
+                        mes = "Abr";
+                        break;
+                    }
+                case 5:
+                    {
+                        mes = "May";
+                        break;
+                    }
+                case 6:
+                    {
+                        mes = "Jun";
+                        break;
+                    }
+                case 7:
+                    {
+                        mes = "Jul";
+                        break;
+                    }
+                case 8:
+                    {
+                        mes = "Ago";
+                        break;
+                    }
+                case 9:
+                    {
+                        mes = "Sep";
+                        break;
+                    }
+                case 10:
+                    {
+                        mes = "Oct";
+                        break;
+                    }
+                case 11:
+                    {
+                        mes = "Nov";
+                        break;
+                    }
+                case 12:
+                    {
+                        mes = "Dic";
+                        break;
+                    }
+               default:
+                   mes="";
+                       break;
+            }
+            return mes;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Enterprise (*.exe)|*.exe";
+            abrir.InitialDirectory = @"\\central\colaboracion\sistemas\enterprise";
+            abrir.Title = "Seleccione el ejecutable NUEVO que va a actualizar";
+            if (abrir.ShowDialog() == DialogResult.OK) 
+            {
+                txtRuta.Text = abrir.FileName;
+            }
+            abrir.Dispose();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+            
+            DateTime x = DateTime.Now;
+            string fecha = x.Day.ToString() + "-" + nombreMes(x.Month) + "-" + x.Year.ToString();
+            string hora = x.Hour.ToString() + "_" + x.Minute.ToString() + "_" + x.Second.ToString();
+
+            string rutaEjecutableActual = @"\\central\ApsRed\ENT-MODULAR\";
+            string nombreEjecutableActual = "EnterpriseModular.exe";
+            string ruta_actual = rutaEjecutableActual + nombreEjecutableActual;
+            string ultimoEjecutable = @"\\central\ApsRed\ENT-MODULAR\UltimoEjecutable\EnterpriseModular " + fecha + " " + hora + ".exe";
+            
+            
+            if (txtRuta.Text != "")
+            {                
+                //aqui hay bug cuando no encuetra el archivo que será renombrado, sin emabargo siempres debe estar porque será el que estén ejecutando.
+                try
+                {
+                    if (chkNotas.Checked)
+                    {                        
+                        File.Move(ruta_actual, ultimoEjecutable + " " + txtNotas.Text + ".exe");
+                    }
+                    else
+                    {
+                        File.Move(ruta_actual, ultimoEjecutable);
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("Error!!! Todavia se encuentra Alguien adentro del " + nombreEjecutableActual + " El proceso será abortado para evitar errores");
+                    error = true;
+                }
+                if (error)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    File.Move(txtRuta.Text, ruta_actual);
+                    MessageBox.Show("El Ejecutable " + nombreEjecutableActual + " fué actualizado con exito.");
+                    txtRuta.Text = "";
+                    error = false;
+                    Application.Exit();
+                } 
+            }
+            else
+            {
+                MessageBox.Show("Debe ubicar ejecutable que va a actualizar primero\n", "Imposible Actualizar. Posibles Causas:");            
+            }
+        }
+
+        private void txtNotas_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkNotas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkNotas.Enabled == true)
+            {
+                txtNotas.Visible = true;
+                txtNotas.Text = "";
+                txtNotas.Focus();
+            }
+            else
+            {
+                txtNotas.Visible = false;
+            }
+        }        
+  }
+}
